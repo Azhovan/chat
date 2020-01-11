62552d1a1720f6c085a03838640b9757e6631292
Azhovan Asadi <azhovan@one.fit>
2020-01-11 22:33:28 +0100

    init commit: set up the skeleton of the application

---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+// make sure all errors are cached
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
