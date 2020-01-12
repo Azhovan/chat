06182487d191d753c0163a2e38598c3c460c8517
Azhovan Asadi <azhovan@one.fit>
2020-01-12 17:31:23 +0100

    encryption class to secure messages

---
+++ src/Encryption/EncrypterInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Chat\Encryption;
+
+
+interface Encrypter
+{
+    /**
+     * Encrypt the given value.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function encrypt($value);
+
+    /**
+     * Decrypt the given value.
+     *
+     * @param  string  $payload
+     * @return string
+     */
+    public function decrypt($payload);
+}
